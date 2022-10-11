79c34caed0e3e6b2b26474092fbc97f1816898bb
Andrija Pavlovic <pavlovicandrija733@gmail.com>
2022-10-11 22:15:17 +0200

    Auth Routes test

---
+++ src/tests/AuthRoutesTests.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AuthRoutesTests extends BasicTest {
+	@Test(priority = 100)
+	public void forbidsVisitsToHomeUrlIfNotAuthenticated() {
+		driver.navigate().to(baseUrl + "home");
+		Assert.assertTrue(driver.getCurrentUrl().contains("/login"), "[Error] URL does not contain /login");
+	}
+
+	@Test(priority = 200)
+	public void forbidsVisitsToProfileUrlIfNotAuthenticated() {
+		driver.navigate().to(baseUrl + "profile");
+		Assert.assertTrue(driver.getCurrentUrl().contains("/login"), "[Error] URL does not contain /login");
+	}
+
+	@Test(priority = 300)
+	public void forbidsVisitsToAdminCitiesUrlIfNotAuthenticated() {
+		driver.navigate().to(baseUrl + "admin/cities");
+		Assert.assertTrue(driver.getCurrentUrl().contains("/login"), "[Error] URL does not contain /login");
+	}
+
+	@Test(priority = 400)
+	public void forbidsVisitsToAdminUsersUrlIfNotAuthenticated() {
+		driver.navigate().to(baseUrl + "admin/users");
+		Assert.assertTrue(driver.getCurrentUrl().contains("/login"), "[Error] URL does not contain /login");
+	}
+}
