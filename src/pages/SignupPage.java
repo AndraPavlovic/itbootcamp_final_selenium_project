112a5ee5fc964089c096910081e56753c3c19a3c
Andrija Pavlovic <pavlovicandrija733@gmail.com>
2022-10-11 17:49:23 +0200

    Commiting pages for final project

---
+++ src/pages/SignupPage.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class SignupPage {
+	private WebDriver driver;
+
+	public SignupPage(WebDriver driver) {
+
+		this.driver = driver;
+	}
+
+	public WebElement getNameInputField() {
+		return driver.findElement(By.id("name"));
+	}
+
+	public WebElement getEmailInputField() {
+		return driver.findElement(By.id("email"));
+	}
+
+	public WebElement getPasswordInputField() {
+		return driver.findElement(By.id("password"));
+	}
+
+	public WebElement getConfirmPasswordInputField() {
+		return driver.findElement(By.id("confirmPassword"));
+	}
+
+	public WebElement getSignUpButton() {
+		return driver.findElement(By.xpath("//*[@type = 'submit']"));
+	}
+}
